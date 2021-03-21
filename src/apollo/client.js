020c044b75e63463f714009359a1bccba7a63c79
Navid <navidadelpour@gmail.com>
2021-03-21 18:27:42 +0330

    use production instead of staging api

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,7 +2,7 @@ import {ApolloClient} from '@apollo/client';
 import cache from './cache';
 
 const client = new ApolloClient({
-  uri: 'https://metaphysics-staging.artsy.net',
+  uri: 'https://metaphysics-production.artsy.net',
   cache,
 });
 
