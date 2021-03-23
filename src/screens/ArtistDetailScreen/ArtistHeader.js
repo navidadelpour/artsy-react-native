7516360e80abd90489ad36ce6e0f87acb834403a
Navid <navidadelpour@gmail.com>
2021-03-24 00:44:57 +0430

    artist header style enhancement

--- src/screens/ArtistDetailScreen/ArtistHeader.js
+++ src/screens/ArtistDetailScreen/ArtistHeader.js
@@ -2,12 +2,18 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Caption, Paragraph, Title, useTheme} from 'react-native-paper';
+import {Caption, Paragraph, Subheading, useTheme} from 'react-native-paper';
 
 const styles = StyleSheet.create({
   wrapper: {
     display: 'flex',
     alignItems: 'center',
+  },
+  subheading: {
+    fontWeight: 'bold',
+  },
+  noVerticalMargin: {
+    marginVertical: 0,
   },
 });
 
@@ -15,14 +21,14 @@ export default function ArtistHeader({name, nationality, birthday, location}) {
   const theme = useTheme();
   return (
     <View style={[styles.wrapper, {backgroundColor: theme.colors.surface}]}>
-      <Title>{name}</Title>
-      <Paragraph>
+      <Subheading style={[styles.subheading, styles.noVerticalMargin]}>{name}</Subheading>
+      <Paragraph style={styles.noVerticalMargin}>
         {nationality}
         {nationality && birthday && ', '}
         {birthday && `b ${birthday}`}
       </Paragraph>
       {location ? (
-        <Caption>
+        <Caption style={styles.noVerticalMargin}>
           <Icon name="location-on" />
           {location}
         </Caption>
