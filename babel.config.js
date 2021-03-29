144475371f0c25511b257446f0462223aa58cc89
Navid <navidadelpour@gmail.com>
2021-03-29 13:50:44 +0430

    absolute import

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ios.js', '.android.js'],
+      },
+    ],
+  ],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
