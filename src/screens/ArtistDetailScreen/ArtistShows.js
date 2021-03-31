49f72d83c757bb0976b638943b1459f6b3122c04
Navid <navidadelpour@gmail.com>
2021-03-31 20:02:40 +0430

    memoziation & improve flat list in ArtistShows

--- src/screens/ArtistDetailScreen/ArtistShows.js
+++ src/screens/ArtistDetailScreen/ArtistShows.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {memo} from 'react';
 import {FlatList, StyleSheet, View} from 'react-native';
 import {Caption, Card, Subheading} from 'react-native-paper';
 import {useNavigation} from '@react-navigation/native';
@@ -9,6 +9,7 @@ const styles = StyleSheet.create({
   card: {
     marginRight: 16,
     marginBottom: 16,
+    height: 250,
   },
   list: {
     paddingTop: 16,
@@ -24,21 +25,6 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function ArtistShows({shows}) {
-  if (!shows || shows.length === 0) {
-    return <BasicIconMessage message="No shows." icon="error-outline" />;
-  }
-
-  return (
-    <FlatList
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={styles.list}
-      data={shows}
-      renderItem={props => <ArtistShow {...props} />}
-    />
-  );
-}
-
 function ArtistShow({item}) {
   const navigation = useNavigation();
   const {id, name, cover_image: image, city, status, exhibition_period} = item;
@@ -52,7 +38,7 @@ function ArtistShow({item}) {
     <Card onPress={onPress} style={styles.card}>
       <Card.Cover source={{uri: url}} />
       <Card.Content style={styles.cardContent}>
-        {name ? <Subheading>{name}</Subheading> : null}
+        {name ? <Subheading numberOfLines={1}>{name}</Subheading> : null}
         <View style={styles.captionWrapper}>
           <Caption>
             {city}
@@ -63,5 +49,43 @@ function ArtistShow({item}) {
         </View>
       </Card.Content>
     </Card>
+  );
+}
+
+const ArtistShowMemoized = memo(ArtistShow);
+
+function getItemLayout(data, index) {
+  const {height, marginBottom} = styles.card;
+  const length = height + marginBottom;
+  const offsetFromTop = styles.list.paddingTop;
+
+  return {
+    length,
+    offset: length * index + offsetFromTop,
+    index,
+  };
+}
+
+function keyExtractor(item) {
+  return item.id;
+}
+
+export default function ArtistShows({shows}) {
+  if (!shows || shows.length === 0) {
+    return <BasicIconMessage message="No shows." icon="error-outline" />;
+  }
+
+  return (
+    <FlatList
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={styles.list}
+      data={shows}
+      renderItem={props => <ArtistShowMemoized {...props} />}
+      initialNumToRender={2}
+      maxToRenderPerBatch={1}
+      windowSize={5}
+      getItemLayout={getItemLayout}
+      keyExtractor={keyExtractor}
+    />
   );
 }
