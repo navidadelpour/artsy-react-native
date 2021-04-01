3e0bf029f2d8566c81364b725b3258da4f51626c
Navid <navidadelpour@gmail.com>
2021-04-01 12:42:34 +0430

    use activityloader of react-native insteadof paper

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,6 @@
 import React, {memo} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {ActivityIndicator, useTheme} from 'react-native-paper';
+import {StyleSheet, ActivityIndicator} from 'react-native';
+import {useTheme} from 'react-native-paper';
 
 const styles = StyleSheet.create({
   loader: {
@@ -13,9 +13,11 @@ const styles = StyleSheet.create({
 function Loader({style}) {
   const theme = useTheme();
   return (
-    <View style={[styles.loader, style]}>
-      <ActivityIndicator color={theme.colors.text} />
-    </View>
+    <ActivityIndicator
+      style={[styles.loader, style]}
+      color={theme.colors.text}
+      size="large"
+    />
   );
 }
 
